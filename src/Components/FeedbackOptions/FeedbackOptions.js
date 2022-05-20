a1c8270587d1b249ef2cc813410f9382f4ffc571
Алексей <fortuk22@rambler.ru>
2022-05-21 00:09:36 +0300

    замечания

--- src/Components/FeedbackOptions/FeedbackOptions.js
+++ src/Components/FeedbackOptions/FeedbackOptions.js
@@ -3,15 +3,16 @@ import PropTypes from 'prop-types';
 import styles from './FeedbackOptions.module.css';
 
 const OptionsFeedback = ({ options, clickFeedback }) => {
-    return options.map((option) => ( < button key = { option }
-        className = { styles.btn }
-        onClick = {
-            () => clickFeedback(option) } >
-        { option } <
-        /button>
+    return options.map((option) => ( 
+        <button
+            key={option}
+            className={styles.btn}
+            onClick={() => clickFeedback(option)}
+        >
+            {option}
+        </button>
     ));
 };
-
 OptionsFeedback.propTypes = {
     options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
     clickFeedback: PropTypes.func.isRequired,
